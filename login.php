59824d0fda83f5116dd40809957139ed2a8b65c9
barathex4 <139451970+barathex4@users.noreply.github.com>
2023-08-11 10:35:11 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+  $usr=$_POST["usr"];
+  $pwd=$_POST["pwd"];
+if($usr=="barath"&& $pwd=="barath143")
+{
+  $loginName="Welcome Admin";
+}
+else
+{
+  $loginName="Login Failed";
+}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login Page</title>	
+		<link rel="stylesheet" href="sitestyle.css">	
+	</head>
+	<body>		
+		<header>
+			<h1>Cloud Login Demo</h1>
+		</header>	
+		<hr>
+		<article>			
+			<section>
+				<h2>Login Details</h1>
+                <form action="login.php" method="POST">
+                    <label>User Name</label>
+                    <input type="text" name="usr"><br><br>
+                    <label>Password</label>
+                    <input type="password" name="pwd"><br>
+                    <input type="submit" value="Login">
+                </form>
+			</section>
+		</article>
+		<footer>
+			Developed by K barath
+		</footer>
+	</body>
+</html>
+
+
